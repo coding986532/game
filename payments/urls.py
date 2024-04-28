a08c0f8a65c338b2d4ef9409613963aaa484232f
cs947939 <sufam5@outlook.com>
2024-04-28 18:46:35 -0500

    fsdfsdfsdfsdf

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('listings', views.Listings, name='listings'),
+    path('listings', views.listonsale, name='listings'),
     path('buy', views.buy, name='buy'),
     path('callback', views.callback, name='callback'),
     path('methodselect', views.methodselect, name='methodselect'),
