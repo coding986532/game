01d1f66f6cca4b7ef65d3f25e174c9f698dc6946
coding986532 <97757073+coding986532@users.noreply.github.com>
2024-04-30 19:51:28 +0000

    asdfsd

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
+from .models import Joblisting
 # Register your models here.
 
 class JoblistingAdmin(admin.ModelAdmin):
     list_display = ('Title')
+
+admin.site.register(Joblisting, JoblistingAdmin)
