0b5cb954d8a20912ca744ca0287d59bbd83692f7
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2024-04-29 20:45:47 -0500

    asdfasdf

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home'),
+  #  path('', views.home, name='home'),
     path('listings', views.listonsale, name='listings'),
     path('buy', views.buy, name='buy'),
     path('callback/<int:txid>', views.callback, name='callback'),
