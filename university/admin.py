2b4b9479ccfca120fa97fda26bec7f611dbe3772
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2024-05-01 18:24:42 -0500

    /

--- university/admin.py
+++ university/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . models import Uni_Listing
+from . models import Programs
 
 class Uni_ListingAdmin(admin.ModelAdmin):
     list_display = ('University', 'Program', 'tuition', )
 # Register your models here.
-admin.site.register(Uni_Listing, Uni_ListingAdmin)
+admin.site.register(Programs, Uni_ListingAdmin)
