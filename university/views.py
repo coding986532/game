65d80ebc50973a53337164b225277295b04844ad
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2024-05-01 18:26:10 -0500

    ]

--- university/views.py
+++ university/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from . models import Program
 
 def uni_listing(request):
-    universities = Program.objects.all()
-    return render(request, 'uni-listings.html', {'offerings':universities})
+    offerings = Program.objects.all()
+    return render(request, 'uni-listings.html', {'offerings':offerings})
 
 # Create your views here.
