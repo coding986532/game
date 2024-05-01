6933fd957716611486cc1fba746baa0d467aa310
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2024-05-01 18:04:21 -0500

    mkpkm

--- university/models.py
+++ university/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Uni_Listing(models.Model):
+    University = models.CharField(max_length=100)
+    Program = models.CharField(max_length=100)
+    location_local = models.CharField(max_length=100)
+    location_broad = models.CharField(max_length=100)
+    tuition = models.CharField(max_length=100)
+    Bachelor = models.BooleanField(blank=True)
+    Associate = models.BooleanField(blank=True)
+    Masters = models.BooleanField(blank=True)
+    PhD = models.BooleanField(blank=True)
+    Enrollment_Start = models.CharField(max_length=100)
+    Enrollment_End = models.CharField(max_length=100)
+    Description = models.TextField(max_length=1000)
