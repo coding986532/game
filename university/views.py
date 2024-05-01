8e2532f8689a173152017cb6ded793b955b9bb0b
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2024-05-01 18:26:30 -0500

    =

--- university/views.py
+++ university/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from . models import Program
-
+ 
 def uni_listing(request): 
     offerings = Program.objects.all()
     return render(request, 'uni-listings.html', {'offerings':offerings})
