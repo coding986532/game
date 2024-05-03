69845980defb14d62408d20378c9b18fe904a824
coding2583571 <97715381+coding2583571@users.noreply.github.com>
2024-05-02 20:24:34 -0500

    junk

--- jobs/views.py
+++ jobs/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render
 from .models import Job, Application
-
+from payments.models import Balance
 # Create your views here.
 
 def joblistings(request):
     jobs = Job.objects.all()
     return render(request, 'joblist.html', {'jobs': jobs})
-def jobsapply(request, jobid):
+def jobsapply(request, jobid, User):
     if request.method == 'GET':
-        return render(request, 'eassy.html')
+        return render(request, 'essaey.html')
     jobdata = Job.objects.get(id=jobid)
     model = Application()
     model.Job = jobdata
-    model.User = request.User
+    model.User = User.objects.get(Username=User)
     eassy = request.POST['reason']
     if "i want this job" in eassy:
         model.Status = True
         model.save()
+        model3 = Balance.objects.get(User=model.User)
+        model3.money = model3.money + jobdata.Doctoral_Pay
+        model3.save()
         return render(request, 'congrats.html')
     else:
         model.Status = False
+        model.save()
+        model3 = Balance.objects.get(User=model.User)
+        model3.money = model3.money - 1000
+        model3.save()
         return render(request, 'failed.html')
+    
